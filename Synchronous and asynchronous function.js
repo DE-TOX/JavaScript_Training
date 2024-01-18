4fe1a9121e14a4d14f5939fa6256a8da42a8993e
DE-TOX <djknight0140@gmail.com>
2024-01-18 17:16:00 +0530

    [divyansh] Added: Synchronous and a synchronous javaScript

---
+++ Synchronous and asynchronous function.js
@@ -0,0 +1,75 @@
+// Synchronous Function
+// 1.
+// function f1() {
+//     console.log("f1");
+//   }
+//   function f2() {
+//     console.log("f1");
+//   }
+//   function f3() {
+//     console.log("f2");
+//   }
+  
+//   f1();
+//   f2();
+//   f3();
+
+// 2. Nested Functions
+
+// function f1() {
+//     console.log("last f1");
+//   }
+//   function f2() {
+//     console.log("second f2");
+//     f1();
+//   }
+//   function f3() {
+//     console.log("first f3");
+//     f2();
+
+//   }
+//   f3();
+
+
+
+
+
+// Asynchronous Function
+
+// 1.
+// function f1() {
+//     console.log('f1');
+// }
+
+// function f2() {
+//     console.log('f2');
+// }
+
+// function main() {
+//     console.log('main');
+    
+//     setTimeout(f1, 0);
+    
+//     f2();
+// }
+
+// main();
+
+//2.
+// function f1() {
+//     console.log('f1');
+// }
+
+// function f2() {
+//     console.log('f2');
+// }
+
+// function main() {
+//     console.log('main');
+//     setTimeout(f1,6000)
+//     setTimeout(f1, 1000);
+    
+//     f2();
+// }
+
+// main();

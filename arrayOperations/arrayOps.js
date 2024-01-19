3592753304d445a152d5c96f4a28337cf9acac03
DE-TOX <djknight0140@gmail.com>
2024-01-19 16:52:44 +0530

    [DIVYANSH] Modified: arrayOps-v2

--- arrayOperations/arrayOps.js
+++ arrayOperations/arrayOps.js
@@ -1,10 +1,65 @@
+//Push
+
+// let arr = ["car", "bike", "scooty"]
+// arr.push("ship")
+// console.log(arr);
 
 
-let arr = ["car", "bike", "scooty"]
+//Pop
 
-arr.push("ship")
+// let array = ["car", "bike", "scooty"]
+// array.pop()
+// console.log(array);
 
-arr.pop()
 
-console.log(arr);
+//UnShift
 
+// let array = ["car", "bike", "scooty"]
+// array.unshift("bicycle")
+// console.log(array);
+
+
+//Shift
+
+// let array = ["Bicycle" ,"car", "bike", "scooty"]
+// array.shift()
+// console.log(array);
+
+
+//Splice
+
+// let array = ["Bicycle" ,"car", "bike", "scooty"]
+// array.splice(1,0,"plane","boat")
+// console.log(array);
+
+// let array = ["Bicycle" ,"car", "bike", "scooty"]
+// array.splice(1,2)
+// console.log(array);
+
+
+//Slice
+
+// let array = ["Bicycle" ,"car", "bike", "scooty"]
+// let newarr = array.slice(1,2)
+// console.log(newarr);
+
+
+//Split
+
+// let str = "method adds and removes array elements"
+// console.log(str.split(" "));
+
+
+//concat
+
+// let arr1 = [1,2,3,4]
+// let arr2 = [5,6,7,8]
+// let arr3 = arr1.concat(arr2)
+// console.log(arr3);
+
+
+// let text1 = "Array";
+// let text2 = "Operations";
+// let result = text1.concat(" ",text2);
+
+// console.log(result);
---
+++ arrayOperations/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+let array = ["Bicycle" ,"car", "bike", "scooty"]
+array.slice(1,2)
+console.log(array);

b9a90284fbc3c687d4c6de7572dd3dcfaa4a1a01
DE-TOX <djknight0140@gmail.com>
2024-01-19 20:55:55 +0530

    [DIVYANSH] Modified: ES-file V2

--- es-features/es.js
+++ es-features/es.js
@@ -4,3 +4,66 @@
 // let lname = "Joshi"
 
 // console.log(`My name is ${fname} ${lname}`);
+
+
+//default parameter
+
+// function func(a,b = "ing"){
+//     console.log(`${a}${b}`);
+// }
+// func("laugh")
+
+
+
+//spread_operator
+// let num1 = [2,4,5]
+// let num2 = [1,3,6]
+
+// let num3 = [...num2,...num1]
+
+// console.log(num3);
+
+
+
+// destructuring
+// let numbers = [1, 2, 3, 4, 5, 6];
+
+// let [one, two, ...rest] = numbers;
+
+// console.log(rest);
+
+//doubt
+// var ob1 = {
+//     name : "RAM",
+//     age : 40
+// };
+
+// var ob2 = {
+//     name : "SHYAM",
+//     age : 20
+// };
+
+// var ob3 = {...ob1 , ...ob2}
+// console.log(ob3);
+
+
+
+
+//key value
+
+
+// var obj ={
+//     foo : "bar",
+//     baz: 42
+// }
+// console.log(Object.values(obj));
+
+
+//includes
+
+// let text = "Hello world,welcome to the universe."
+// if (text.includes("Hello")) {
+// console.log("yes i am here!")
+// }
+// else
+// console.log("NO i am not! !");

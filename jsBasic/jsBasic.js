06bcde07005a9d509a3ef7bf9a9e6c8da4883e7e
DE-TOX <djknight0140@gmail.com>
2024-01-20 11:10:39 +0530

    [DIVYANSH] Modified: JS_Basic V1

---
+++ jsBasic/jsBasic.js
@@ -0,0 +1,35 @@
+
+
+// let a=1;
+// function tmp(){
+//     a=10;
+//     console.log(a);
+//     {
+//         a=18;
+//         console.log(a);
+//     }
+// }
+// console.log(a);
+// tmp();
+
+// function cal(){
+//     return a + b;
+// }
+
+// let a = 5
+// let b = 6
+// console.log(cal(a,b))
+
+// var mStr = "This is my test string to practice the JavaScript string function concepts. This is gonna be fun. I love javascript "
+// // 1. Find the length of the first sentence in the string.
+// // 2. Find the length of the second sentence in the string.
+// // 3. Find the first and last occurrence of the word "This".
+// // 4. Find the sub-string with length 12 from the START of the string mStr.
+// // 5. Find the sub-string with length 12 from the END of the string mStr.
+// var firstSentence = mStr.split('.')[0]
+// var secondSentence = mStr.split('.')[1]
+// var lastSentence = mStr.split('.')[-1]
+// console.log(lastSentence)
+// console.log(mStr.substr(0,12))
+
+
